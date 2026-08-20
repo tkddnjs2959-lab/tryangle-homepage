10372577df47cfd82ec4f40a2f0a416a07d17507
김상원 <tkddnjs2959@gmail.com>
2026-08-20 22:01:17 +0900

    Connect Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export const viewport: Viewport = {
 };
 
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1408099347850769';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -44,6 +45,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             `}
           </Script>
         ) : null}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${metaPixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       </head>
@@ -68,6 +83,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             />
           </noscript>
         ) : null}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
         {children}
         <ConsultationCta />
         <Analytics />
